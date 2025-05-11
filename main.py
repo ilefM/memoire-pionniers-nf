61c83da9ba3c21dbdcb27964d40a4cadf0907291
ilefM <ilefikhelefdev1@gmail.com>
2025-05-11 23:12:14 +0200

    charente AI left

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from characters import Character, extract_characters
 from towns import Town, extract_towns
 from mistral import extract_information_bio
 
-DEPARTMENT = "vienne"
+DEPARTMENT = "charente"
 
 def read_file(department):
     dataLines = []
@@ -37,11 +37,11 @@ def main():
         
         characters_partial = extract_characters(town["characters_text"], town["characters"], town["name"])
         for char in characters_partial:
-            data = extract_information_bio(char["bio"])
-            data_list = list(data.values())
-            char["place_of_birth"] = data_list[0]
-            if len(data_list) > 1:
-                char["place_of_death"] = data_list[1]
+            # data = extract_information_bio(char["bio"])
+            # data_list = list(data.values())
+            # char["place_of_birth"] = data_list[0]
+            # if len(data_list) > 1:
+            #     char["place_of_death"] = data_list[1]
             char["principal_place"] = town["name"] + " (" + town["postal_code"] + ")"
             characters.append(char)
 
